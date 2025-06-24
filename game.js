16bb3c2ddea9653184a2ea54bb9617090c1d3d03
Niouk971 <florian.polter.77@gmail.com>
2025-06-24 16:08:28 +0200

    Modif bouton suivant et rejouer

--- game.js
+++ game.js
@@ -1,20 +1,66 @@
 import { quiz } from './questions.js'
 
-const question = document.getElementById("question-text");
-const options = document.getElementById("options-container");
-
 // Récupérer la première question
-const firstQuestion = quiz.questions[0];
+// const firstQuestion = quiz.questions[0];
 
 // Injecter le texte de la question dans l'emplacement dédié
-question.innerText = firstQuestion.text
+// question.innerText = firstQuestion.text
 
 // Pour chaque option, créer un bouton et l'ajouter au conteneur
-firstQuestion.choices.forEach(choice => {
+// firstQuestion.choices.forEach(choice => {
+//   const answer_btn = document.createElement('button');
+//   answer_btn.innerText = choice;
+// //   console.log(choice)
+//   answer_btn.classList.add("options");
+//   //console.log(options.classList.add(choice))
+//   options.appendChild(answer_btn);
+// });
+
+let currentQuestionIndex = 0;
+
+const question = document.getElementById("question-text");
+const optionsAnswers = document.getElementById("options-container");
+const nextButton = document.getElementById("next-button");
+const replayButton = document.getElementById("replay-button");
+
+console.log(question);
+console.log(nextButton);
+console.log(currentQuestionIndex);
+console.log(replayButton);
+
+function loadQuestion() {
+  optionsAnswers.innerHTML = '';
+
+  
+  
+  //console.log(loadQuestion);
+  const currentQuestion = quiz.questions[currentQuestionIndex];
+  console.log(currentQuestion);
+  
+  question.innerText = currentQuestion.text
+  console.log(currentQuestion)
+
+  // Injecter les options dans le HTML 
+  currentQuestion.choices.forEach(choice => {
   const answer_btn = document.createElement('button');
   answer_btn.innerText = choice;
-//   console.log(choice)
-  answer_btn.classList.add("options");
-  //console.log(options.classList.add(choice))
-  options.appendChild(answer_btn);
-});
+  answer_btn.classList.add('options');
+  optionsAnswers.appendChild(answer_btn);
+  });
+}
+
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex ++
+  if (currentQuestionIndex < quiz.questions.length) {
+    loadQuestion();
+  } else {
+    question.innerText = "Le quiz est terminé !";
+    replayButton.classList.add("button-container");
+    optionsAnswers.innerHTML = '';
+    nextButton.style.display = "none";
+    replayButton.style.display = "block";
+  }
+})
+
+loadQuestion()
+
