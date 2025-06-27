57d592205d32170ba9bee7a0f732c0dc9bcea9a7
Niouk971 <florian.polter.77@gmail.com>
2025-06-27 16:09:31 +0200

    Update game.js

--- game.js
+++ game.js
@@ -22,6 +22,7 @@ firstQuestion.choices.forEach(choice => {
 let currentQuestionIndex = 0; //On commence à 0 (la première question)
 let score = 0;
 let compteur = 10
+// let explanationAdd = 0
 
 // &Tous les éléments HTML
 const question = document.getElementById("question-text");//*question
@@ -30,103 +31,112 @@ const nextButton = document.getElementById('next-button');//*le bouton suivant
 const replayButton = document.getElementById('replay-button')//*le bouton rejouer (qui est caché voir html)
 const affichageScore = document.getElementById('affiche-score')//* le score
 const affichageTimer = document.getElementById('affiche-timer')//* le timer
-const progress = document.getElementById("progress")// barre de progression
+const progress = document.getElementById("progress")//* barre de progression
 const explanationText = document.getElementById('explanation')//* les explications
 
-// &Fonction qui affiche une question 
-function loadQuestion() {
+// &Constances
+//currentQuestionIndex++
+//const currentQuestion = quiz.questions[currentQuestionIndex];
 
-	//* On vide les anciennes réponses (si on est à la 2e ou 3e question par exemple)
-	optionsAnswers.innerHTML = '';
-	
-	explanationText.innerText = '';
+//& Fonctions
+	//& Fonction qui affiche une question
+	function loadQuestion() {
 
-	affichageScore.innerText = `Score ${score}/${quiz.questions.length}`
+		//* On vide les anciennes réponses (si on est à la 2e ou 3e question par exemple)
+		optionsAnswers.innerHTML = '';
+		
+		explanationText.innerText = '';
 
-	//* On récupère la question actuelle à partir de l'index
-	const currentQuestion = quiz.questions[currentQuestionIndex];
-	// on affiche la barre de progression des questions
-		progress.innerText = "";
-		quiz.questions.forEach(question => {
-			progress.innerHTML += "<span></span>"
-		})
-	
-		let spans = document.querySelectorAll('span');
-	
-		for (let i = 0; i <= currentQuestionIndex; i++)
-			spans[i].style.backgroundColor = "yellow"
-	//* On affiche le texte de la question dans le HTML
-	question.innerText = currentQuestion.text;
+		affichageScore.innerText = `Score ${score}/${quiz.questions.length}`
 
-	//* On désactive le bouton "Suivant" tant qu'aucune réponse n’est cliquée
-	nextButton.disabled = true;
-
-	//* On crée un bouton pour chaque réponse possible 
-	checkTime()
-	
-	currentQuestion.choices.forEach(choice => {
-
-		const answer_btn = document.createElement('button');// On crée un nouveau bouton pour chaque réponse
-		answer_btn.innerText = choice; // On met le texte du choix dans le bouton
-		answer_btn.classList.add("options");  // On lui donne la classe CSS "options" (pour le style)
-		optionsAnswers.appendChild(answer_btn);  // On ajoute le bouton dans la page HTML
-
-		//&Quand on clique sur une réponse...
-		answer_btn.addEventListener('click', (event) => {
-			const selectedAnswer = event.target.innerText; // On récupère le texte du bouton cliqué
-			console.log(selectedAnswer);// juste pour voir ce que l’utilisateur a cliqué
-
-			let result = checkAnswer(selectedAnswer)
+		//* On récupère la question actuelle à partir de l'index
+		const currentQuestion = quiz.questions[currentQuestionIndex];
+		// explanation.innerText = currentQuestion.explanation;
 
 
-			console.log(result)
-			
-			explanationText.innerText = currentQuestion.explanation
+		//* on affiche la barre de progression des questions
+			progress.innerText = "";
+			quiz.questions.forEach(question => {
+				progress.innerHTML += "<span></span>"
+			})
+		
+			let spans = document.querySelectorAll('span');
+		
+			for (let i = 0; i <= currentQuestionIndex; i++)
+				spans[i].style.backgroundColor = "yellow"
 
-			// On désactive tous les boutons de réponse pour empêcher de cliquer plusieurs fois
-			const allButtons = optionsAnswers.querySelectorAll('.options');
-			allButtons.forEach(btn => btn.disabled = true);
+		//* On affiche le texte de la question dans le HTML
+		question.innerText = currentQuestion.text;
 
-			// Si la réponse est correcte (== la bonne réponse définie dans les données)
-			if (result === true) {
-				event.target.style.border = 'solid green';// On met le bouton en vert
-				score++
-				console.log(score)
-				affichageScore.innerText = `Score ${score}/${quiz.questions.length}`
-			} else {
-				event.target.style.border = 'solid red'; // Sinon, on met le bouton cliqué en rouge
-				console.log(score)
-				// Et on cherche le bouton qui contient la bonne réponse, pour le colorer en vert
-				//const correctBtn = Array.from(allButtons).find(btn => btn.innerText === currentQuestion.correctAnswer);
-				allButtons.forEach(btn => {
-					if (btn.innerText === currentQuestion.correctAnswer) {
-						btn.style.border = 'solid green';
-					}
-				})
-			}
+		//* On désactive le bouton "Suivant" tant qu'aucune réponse n’est cliquée
+		nextButton.disabled = true;
 
-			// Une fois qu'on a répondu, on autorise à cliquer sur "Suivant"
-			nextButton.disabled = false;
-		});// Fin de la boucle forEach (création des boutons)
-	});// Fin de la fonction loadQuestion
-}
+		//* On crée un bouton pour chaque réponse possible 
+		checkTime()
+		
+		currentQuestion.choices.forEach(choice => {
 
-//Quand on clique sur le bouton "Suivant"
-	nextButton.addEventListener('click', () => {
+			const answer_btn = document.createElement('button');// On crée un nouveau bouton pour chaque réponse
+			answer_btn.innerText = choice; // On met le texte du choix dans le bouton
+			answer_btn.classList.add("options");  // On lui donne la classe CSS "options" (pour le style)
+			optionsAnswers.appendChild(answer_btn);  // On ajoute le bouton dans la page HTML
 
-	// On passe à la question suivante en augmentant l'index
-	currentQuestionIndex++;
-	affichageTimer.innerText = ''
-	// S’il reste des questions, on les affiche
-	if (currentQuestionIndex < quiz.questions.length) {
-		loadQuestion();
-	} else { // Sinon, on affiche un message de fin de quiz
-		question.innerText = 'fin du quiz';
-		optionsAnswers.innerHTML = '';// on vide les réponses
-		nextButton.style.display = 'none';// on cache le bouton "suivant"
-		replayButton.style.display = 'block';// on montre le bouton "rejouer"
+			//&Quand on clique sur une réponse...
+			answer_btn.addEventListener('click', (event) => {
+				const selectedAnswer = event.target.innerText; // On récupère le texte du bouton cliqué
+				console.log(selectedAnswer);// juste pour voir ce que l’utilisateur a cliqué
+
+				let result = checkAnswer(selectedAnswer)
+
+				console.log(result)
+				
+				explanationText.innerText = currentQuestion.explanation
+
+				// On désactive tous les boutons de réponse pour empêcher de cliquer plusieurs fois
+				const allButtons = optionsAnswers.querySelectorAll('.options');
+				allButtons.forEach(btn => btn.disabled = true);
+
+				// Si la réponse est correcte (== la bonne réponse définie dans les données)
+				if (result === true) {
+					event.target.style.border = 'solid green';// On met le bouton en vert
+					score++
+					console.log(score)
+					affichageScore.innerText = `Score ${score}/${quiz.questions.length}`
+				} else {
+					event.target.style.border = 'solid red'; // Sinon, on met le bouton cliqué en rouge
+					console.log(score)
+					// Et on cherche le bouton qui contient la bonne réponse, pour le colorer en vert
+					//const correctBtn = Array.from(allButtons).find(btn => btn.innerText === currentQuestion.correctAnswer);
+					allButtons.forEach(btn => {
+						if (btn.innerText === currentQuestion.correctAnswer) {
+							btn.style.border = 'solid green';
+						}
+					})
+				}
+
+				// Une fois qu'on a répondu, on autorise à cliquer sur "Suivant"
+				nextButton.disabled = false;
+			});// Fin de la boucle forEach (création des boutons)
+		});// Fin de la fonction loadQuestion
 	}
-})
+
+	//Quand on clique sur le bouton "Suivant"
+		nextButton.addEventListener('click', () => {
+
+		// On passe à la question suivante en augmentant l'index
+		currentQuestionIndex++;
+		affichageTimer.innerText = ''
+		// S’il reste des questions, on les affiche
+		if (currentQuestionIndex < quiz.questions.length) {
+			loadQuestion();
+		} else { // Sinon, on affiche un message de fin de quiz
+			question.innerText = 'fin du quiz';
+			optionsAnswers.innerHTML = '';// on vide les réponses
+			nextButton.style.display = 'none';// on cache le bouton "suivant"
+			replayButton.style.display = 'block';// on montre le bouton "rejouer"
+			explanationText.innerText = ''
+		}
+	})
 	
 
 //Quand on clique sur le bouton "Rejouer" :
@@ -155,25 +165,56 @@ function checkAnswer(response) {
 function checkTime() {
 	compteur = 10
 	let id = setInterval(() => {
+		
 		if (compteur === 0) {
+			
 			clearInterval(id);
-
+			
+			const currentQuestion = quiz.questions[currentQuestionIndex];
+			if(currentQuestionIndex<=quiz.questions.length){
 			const allButtons = document.querySelectorAll('.options');
 			allButtons.forEach((btn) => {
 				btn.disabled = true
-				// if check answer btn.innerText => 
+				nextButton.disabled = false
+				// if check answer btn.innerText =>
+				
+				
+				explanationText.innerText = currentQuestion.explanation;
 				if(checkAnswer(btn.innerText)===true){
 					btn.style.border = 'solid green';
-				}else{
-					btn.style.border = 'solid red'	
+					explanationText.innerText = currentQuestion.explanation;
+					//explanationText.innerText = ''
+					 
+					//explanationText.innerText = ''
 				}
+				
 			});
-			
-			//nextButton.click()
-		}
+			//suivantButton()
+			 //console.log(setTimeout(()=>{suivantButton(),10000}))
+		}}
+		
 		console.log(compteur)
 		affichageTimer.innerText = compteur
 		compteur--;
+		
 	}, 1000)
+	//let startNumber = 0
+	//setTimeout(() => { clearInterval(startNumber)}, 10000)
+	
 }
 
+function suivantButton(){
+	// On passe à la question suivante en augmentant l'index
+		currentQuestionIndex++;
+		affichageTimer.innerText = ''
+		// S’il reste des questions, on les affiche
+		if (currentQuestionIndex < quiz.questions.length) {
+			loadQuestion();
+		} else { // Sinon, on affiche un message de fin de quiz
+			question.innerText = 'fin du quiz';
+			optionsAnswers.innerHTML = '';// on vide les réponses
+			nextButton.style.display = 'none';// on cache le bouton "suivant"
+			replayButton.style.display = 'block';// on montre le bouton "rejouer"
+			explanationText.innerText = ''
+		}
+}
